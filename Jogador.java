c1bf6afee4df1a0c408c9f375a88686a6763c4c5
JuliaSalvi <julia.sg.mr@gmail.com>
2023-09-20 14:50:09 -0300

    adicionar e remover jogador

--- Jogador.java
+++ Jogador.java
@@ -1,8 +1,28 @@
 public class Jogador {
     private String nome = new String();
     private String tipo = new String();
+    private JogoGeneral jogo = new JogoGeneral();
 
-    public Jogador 
+    public Jogador(String nome, String tipo){
+        this.nome = nome;
+        this.tipo = tipo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+    
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
 
 
     
