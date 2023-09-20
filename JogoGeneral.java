5d024c0af6ca4ba54273f611e590ecaf569cbcb3
Carweni <caroline_ms2004@hotmail.com>
2023-09-20 15:14:31 -0300

    JogoGeneral e Dado atualizados

--- JogoGeneral.java
+++ JogoGeneral.java
@@ -22,13 +22,19 @@ public class JogoGeneral {
         }
     }
 
-    public void toString(){ // Imprime os valores dos dados.
+    public String toString(){ // Imprime os valores dos dados.
         int i;
+        String str = new String();
 
         System.out.println("Valores dos dados:");
         for(i = 0; i < 5; i++){
-            System.out.print(dice[i] + " ");
+            str.append(dice[i]);
+            if (i < 4){
+                str.append("-");
+            }
         }
+
+        return str;
     }
 
     public boolean validarJogada(int n){
@@ -41,10 +47,10 @@ public class JogoGeneral {
     }
 
     public int pontuarJogada(int n){
-        int i;
+        int i, j;
         int[] armazenaValores = new int[5];
 
-        for(i = 0; i < 5; i++){
+        for(i = 0; i < 6; i++){
             armazenaValores[dice[i].getFaceSuperior() - 1] += 1;
         }
 
@@ -67,25 +73,98 @@ public class JogoGeneral {
             return armazenaValores[5] * 1;
         }
         else if(n == 7){
-            
+            int soma = 0;
+
+            for(i = 0; i < 6; i++){
+                if(armazenaValores[i] == 3){
+                    for(j = 0; j < 5; j++){
+                        soma += dice[i].getFaceSuperior();
+                    }
+                    break;
+                }
+            }
+
+            return soma;
         }
         else if(n == 8){
+            int sum = 0;
+
+            for(i = 0; i < 6; i++){
+                if(armazenaValores[i] == 3){
+                    for(j = 0; j < 5; j++){
+                        sum += dice[i].getFaceSuperior();
+                    }
+                    break;
+                }
+            }
+
+            return sum;
             
         }
         else if(n == 9){
-            
+            for(i = 0; i < 6; i++){
+                if(armazenaValores[i] == 3){
+                    for(j = 0; j < 6; j++){
+                        if(armazenaValores[i] == 2){
+                            return 25;
+                        }
+                    }
+                    break;
+                }
+            }
+
+            return 0;
         }
         else if(n == 10){
+            int cont = 0;
+
+            for(i = 1; i < 6; i++){
+                if(armazenaValores[i] == 1){
+                    cont += 1;
+                }
+                else{
+                    return 0;
+                }
+            }
             
+            if(cont == 5){
+                return 30;
+            }
         }
         else if(n == 11){
+            int c = 0;
+
+            for(i = 0; i < 5; i++){
+                if(armazenaValores[i] == 1){
+                    c += 1;
+                }
+                else{
+                    return 0;
+                }
+            }
             
+            if(c == 5){
+                return 40;
+            }
         }
         else if(n == 12){
-            
+            int c = 0;
+
+            for(i = 0; i < 6; i++){
+                if(armazenaValores[i] == 5){
+                    return 50;
+                }
+            }
+
+            return 0;
         }
         else if(n == 13){
-            
+            int s = 0;
+
+            for(j = 0; j < 5; j++){
+                s += dice[i].getFaceSuperior();
+            }
+            return s;
         }
     }
     
