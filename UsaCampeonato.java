02733dfc25f9dd5ec371602cf500ae1c1da2474b
JuliaSalvi <julia.sg.mr@gmail.com>
2023-10-04 14:58:31 -0300

    comentando

--- UsaCampeonato.java
+++ UsaCampeonato.java
@@ -36,7 +36,7 @@ public class UsaCampeonato {
                     int n = league.jogadorLivre();
                     String nome;
 
-                    if(n < league.getLength()){
+                    if(n < league.getLength()){ //Verifica se não atingiu numero maximo de jogadores
                         do{
                             System.out.println("Informe o apelido(nickname) do jogador a ser adicionado: ");
                             nome = tec.nextLine();
@@ -89,7 +89,7 @@ public class UsaCampeonato {
                 case 'g': // Se a escolha foi 'g', sai da aplicacao:
                     sair = 1;
                     break;
-                case 'm':
+                case 'm': // Se a escolha foi 'm', mostra-se o menu novamente:
                     System.out.println(":..::..: Menu interativo :..::..: ");
                     System.out.println("a - Incluir jogador");
                     System.out.println("b - Remover jogador");
